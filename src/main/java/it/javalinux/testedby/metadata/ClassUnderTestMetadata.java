a1c1d999a9b2de274c422d85a01bc43de71c30ab
maestri.stefano <maestri.stefano@fca47524-2756-0410-b7c8-b385b6c05b64>
2009-08-12 09:05:10 +0000

    defining metadata interfaces
    git-svn-id: https://testedby.googlecode.com/svn/trunk@16 fca47524-2756-0410-b7c8-b385b6c05b64

---
+++ src/main/java/it/javalinux/testedby/metadata/ClassUnderTestMetadata.java
@@ -0,0 +1,55 @@
+/*
+ * Stefano Maestri, javalinuxlabs.org Copyright 2008, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors. 
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package it.javalinux.testedby.metadata;
+
+import it.javalinux.testedby.metadata.builder.MetadataBuilder;
+
+import java.util.Collection;
+
+/**
+ * It represent metadatas for ClassUnderTest.
+ * It contains both its own metadata (accessed by {@link #getTestClasses()} and {@link #getTestMethods()} )
+ * and a Colection of {@link MethodUnderTestMetadata} (accessed by {@link #getMethodsSpecificMetaDatas()})
+ * 
+ * It aims to collect metadata to make possible navigation of relation between class under test and test classes/methods
+ * using class under test as starting point.
+ * 
+ * Metadata will be created by a {@link MetadataBuilder} with a specific strategy
+ * 
+ * @author stefano.maestri@javalinux.it
+ *
+ */
+public interface ClassUnderTestMetadata extends CodeUnderTestMetadata {
+	
+	
+	/**
+	 * 
+	 * @return the {@link Class} under test
+	 */
+	public Class<?> getClassUnderTest();
+	
+	/**
+	 * 
+	 * @return a Colection of {@link MethodUnderTestMetadata} representing metadata specific of methods of the class under test
+	 */
+	public Collection<MethodUnderTestMetadata> getMethodsSpecificMetaDatas(); 
+
+}
---
+++ src/main/java/it/javalinux/testedby/metadata/CodeUnderTestMetadata.java
@@ -0,0 +1,47 @@
+/*
+ * Stefano Maestri, javalinuxlabs.org Copyright 2008, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors. 
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package it.javalinux.testedby.metadata;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+/**
+ * It represent metadatas for a unit of code under test (i.e class under test and method under test)
+ * It contains generic metadata for any level (accessed by {@link #getTestClasses()} and {@link #getTestMethods()} )
+ *
+ * 
+ * @author stefano.maestri@javalinux.it
+ *
+ */
+public interface CodeUnderTestMetadata {
+
+	/**
+	 * 
+	 * @return a Colection of {@link Class} that are the test class stressing the represented class under test
+	 */
+	public Collection<Class<?>> getTestClasses();
+
+	/**
+	 * 
+	 * @return a Colection of {@link Method} that are the method of test class stressing the represented class under test
+	 */
+	public Collection<Method> getTestMethods();
+}
